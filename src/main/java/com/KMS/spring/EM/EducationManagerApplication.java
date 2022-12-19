0e3ffee73b86968549d4ca990848c1de8668f888
casatoo <casato6666@gmail.com>
2022-12-19 16:29:16 +0900

    프로젝트 배포 설정 중

--- src/main/java/com/KMS/spring/EM/EducationManagerApplication.java
+++ src/main/java/com/KMS/spring/EM/EducationManagerApplication.java
@@ -2,12 +2,18 @@ package com.KMS.spring.EM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class EducationManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EducationManagerApplication.class, args);
+	}
+	
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EducationManagerApplication.class);
 	}
 
 }
